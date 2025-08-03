74e536bc9f04ccc54d63ca889bb6065c9c55dcea
Ruben van Nieuwpoort <ruben@vannieuwpoort.dev>
2025-08-03 20:53:49 +0200

    Implement cross-entropy loss

--- neural_network/loss_functions/__init__.py
+++ neural_network/loss_functions/__init__.py
@@ -1,3 +1,4 @@
 from neural_network.loss_functions.mean_square_error import MeanSquareError
+from neural_network.loss_functions.cross_entropy_loss import CrossEntropyLoss
 
-__all__ = ['MeanSquareError']
+__all__ = ['MeanSquareError', 'CrossEntropyLoss']
